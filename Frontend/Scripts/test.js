b9aa6f3a4ff6b22d63360c0c969bad4a3eac960a
Elucid503 <paulujlaky@gmail.com>
2024-04-03 22:52:59 -0400

    Crappy video thing, really not good

--- Frontend/Scripts/test.js
+++ Frontend/Scripts/test.js
@@ -1,12 +1,10 @@
-let FRAMES = 315;
+let FRAMES = 314;
 let FPS = 30;
 let video = document.getElementById('video');
 
 window.addEventListener('scroll', function (e) {
 
     let time = (window.scrollY / 1000) * FRAMES / FPS;
-
-    console.log(time);
 
     video.currentTime = time;
     
